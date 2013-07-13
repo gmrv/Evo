abda80335f308bf690df7881095e611dcca5d64b
sh gumerov <sh.gumerov@bdo.ru>
2013-07-14 00:23:57 +0400

    trash test

---
+++ trash/java/src/gsf/TrashTest.java
@@ -0,0 +1,14 @@
+package gsf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GUMEROV_SHF
+ * Date: 14.07.13
+ * Time: 0:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class TrashTest {
+    public static void main(String[] args) {
+        System.out.println("qwerty");
+    }
+}
